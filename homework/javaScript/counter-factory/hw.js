ff3373057b1da470092a3bc38c52579256b0ebd6
smpr <gcbrown1116@gmail.com>
2017-09-08 07:53:15 -0400

    thursday homework

--- homework/javaScript/counter-factory/hw.js
+++ homework/javaScript/counter-factory/hw.js
@@ -49,11 +49,23 @@ const CounterCollection = {
 const Presenter = {
   insertCounterComponent: function(newCountId){
     console.log(`insert counter component #${newCountId}`);
-    // Your Code Here
+    let newtable = document.createElement('div');
+    newtable.innerHTML = `<h3>Count: <span>0</span></h3>
+                          <button class='increment'> + 1 </button>`;
+    console.log('newtbale,',newtable);
+    newtable.dataset.counterid = newCountId;
+    newtable.getElementsByClassName('increment')[0].onClick= AppController.onClickIncrement;
+    document.getElementById('counter-list').appendChild(newtable);
+    console.log('worked')
+    // had to check the solution for this, but i actually wasnt aware that the background needed to be changed with each new counter. this code sets the class to counter which is defined in css
+    newtable.className = "counter";
+      // i was not able to figure out how to get the counter to increment its number. i thought it would be simply like adding a click event that just did an addition to the count: 0 sting but i wasnt able to figure out how. 
+    
   },
   refreshCounterComponent: function(countId){
     console.log(`refresh counter component #${countId}`);
-    // Your Code Here
+    let val = CounterCollection.getCounterValue(countId);
+    document.querySelector(`[data-count-id="${countId}"] span`).innerHTML = val;
   },
   removeCounterComponent: function(countId){             // REACH
     console.log(`remove counter component #${countId}`);
@@ -64,13 +76,20 @@ const Presenter = {
 // Top-Level Application Control //
 const AppController = {
   onClickNewCounter: function(event){
-    // Your Code Here
-  },
+    // adds more counters. ?
+    CounterCollection.createCounter()
+    Presenter.insertCounterComponent(CounterCollection.lastCountId);
+
+},
   onClickIncrement: function(event){
-    // Your Code Here
+    let countId = Number(event.target.parentNode.dataset.countId);
+    console.log(`click increment #${countId}`);
+    CounterCollection.incrementCounter(countId);
+    Presenter.refreshCounterComponent(countId);
   },
   onClickDelete: function(event){                           // REACH
     // Your Code Here
+    //should be a remove child function 
   }
 };
 
