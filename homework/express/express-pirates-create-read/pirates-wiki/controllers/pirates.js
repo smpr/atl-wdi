a373dece99dd9910906edbecf39eb5be1042a346
smpr <gcbrown1116@gmail.com>
2017-09-21 16:51:48 -0400

    more screwing up

--- homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -2,13 +2,13 @@
 //requirements
 var express = require("express");
 var router = express.Router();
-var pirates = require('../models/pirates.js');
+//var pirates = require('../models/pirates.js');
 const bodyParser = require('body-parser')
 
 //all routes for /pirate
 //router.get()
 //router.post(......)
-router.get('/new', (req, res) => {
+router.get('../new', (req, res) => {
     res.render('views/new');
   })
 router.use(bodyParser.urlencoded({ extended: false }));
--- homework/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/express/express-pirates-create-read/pirates-wiki/server.js
@@ -2,15 +2,23 @@
 var path        = require('path');
 var express     = require('express');
 var hbs         = require('hbs');
+var port        = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
 const app = express();
 var pirateController = require('./controllers/pirates.js');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'hbs');
 
 app.use("/pirates", pirateController);
 
-var port        = process.env.PORT || 3000;
 
+
+/* HOME */
+app.get('/', function(req,res) {
+  res.send('This is our Home Page');
+});
 app.listen(port, function() {
     console.info('Arrg.....', port,"//", new Date());
   });
