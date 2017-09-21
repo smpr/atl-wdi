9d9bf10d6ceb7d70c2038cd4ff8275b064bb1390
smpr <gcbrown1116@gmail.com>
2017-09-21 08:49:49 -0400

    Lost power and files :(

--- homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,15 @@
 //your code below
+//requirements
+var express = require("express");
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+//all routes for /pirate
+//router.get()
+//router.post(......)
+
+router.get('/', function(req,res){
+    res.send('Argggg')
+})
+//exports
+module.exports = router;
--- homework/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,16 @@
 //your code here
+var path        = require('path');
+var express     = require('express');
+var hbs         = require('hbs');
+const bodyParser = require('body-parser')
+const app = express();
+var pirateController = require('./controllers/pirates.js');
+
+
+app.use("/pirates", pirateController);
+
+var port        = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.info('Arrg.....', port,"//", new Date());
+  });
