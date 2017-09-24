9279383d77108f9fba25ab362169ae5ac00054f1
smpr <gcbrown1116@gmail.com>
2017-09-24 13:30:38 -0400

    part1:screated constructor

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -1,9 +1,22 @@
 console.log('tamagotchi file is loaded');
 
 //your class declaration here
-
+class Pet {
+    constructor(){
+        this.foodInTummy = 10;
+        this.restedness = 10;
+        this.health = 10;
+    }
+    cry(){
+        this.foodInTummy--;
+        console.log('food level '+this.foodInTummy);
+        console.log('Feed me!')
+    }
+}
 
 //create new Tamagotchis
-
-
+const newPet1 = new Pet();
+const newPet2 = new Pet();
 //test out your Tamagotchies below via console.logs
+newPet1.cry();
+newPet2.cry();
