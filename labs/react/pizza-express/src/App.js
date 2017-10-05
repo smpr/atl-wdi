41bddb1af57a366d84fe9d94cf88d1bd1715f823
smpr <gcbrown1116@gmail.com>
2017-10-05 18:02:36 -0400

    homework complete

--- labs/react/pizza-express/src/App.js
+++ labs/react/pizza-express/src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-
+import './index.css';
+import Description from './Components/Description.jsx'
+import Contact from './Components/Contact.jsx'
+import Menu from './Components/MenuList.jsx'
+import Locations from './Components/LocationList.jsx'
+import NavBar from './Components/Navbar.jsx'
+import SplashImage from './Components/Splashimage.jsx'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Place your components in here!</h1>
+        <NavBar />
+        <SplashImage />
+        <Description />
+        <Menu />            
+        <Locations />
+        <Contact />
+         
       </div>
     );
   }
