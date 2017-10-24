015e9f8c2541c7265825fd31040daf1794c3050a
smpr <gcbrown1116@gmail.com>
2017-10-24 09:02:47 -0400

    #3

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -10,4 +10,25 @@
 # # 4. Write a statement using a and b that evaluates to true
 #     a != b 
 #     puts a != b
-# #       puts the result to the terminal
+# Challenge 2: 
+
+## Challenge 2: Nil
+#test = NIL
+
+#puts test
+#puts "#{test} was here"
+#print test
+#puts test.class
+#3 integers
+##1 
+d = 10
+puts d
+##2
+e = 5.5
+puts e
+##3
+d = 9
+e = 3.5
+puts d
+puts e
+## 4
